94b5d3b32476b5a3895a92392530ec14ef72904c
hakther15 <92819734+hakther15@users.noreply.github.com>
2022-02-17 21:24:52 -0500

    added axios and connected an api url

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="container">
+    <div className="container">
+      <h1 className="dictionary">What does it mean??</h1>
+      <h4 className="sub-title">Dictionary search</h4>
+      <div className="App">
         <Dictionary />
-      </header>
+      </div>
+      <div className="github">
+        <a
+          href="https://github.com/hakther15/dictionary-react-app"
+          target="blank">
+          Open-source code
+        </a>{" "}
+        by Sina A
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
     let [word, setWord] = useState(null);
+
+    function handleResponse(response) {
+        console.log(response.data[0]);
+
+    }
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${word}...`);
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleWord(event) {
