cf14bfde52382d1ac660a3f2ff5186b8139bb932
hakther15 <92819734+hakther15@users.noreply.github.com>
2022-02-23 22:22:59 -0500

    Added more CSS to the code and created a default word for the dictionary app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,29 +2,45 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() {
-    let [word, setWord] = useState("");
+export default function Dictionary(props) {
+    let [word, setWord] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
-    function search(event) {
-        event.preventDefault();
+
+    function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
     function handleWord(event) {
         setWord(event.target.value);
 
     }
-    return (
-        <div className="search">
-            <form onSubmit={search}>
-                <input className="search-bar" placeholder="Type a word" size="50" type="search" autoFocus={true} onChange={handleWord} />
-            </form>
-            <Results results={results} />
-        </div>
-    );
+
+    function load() {
+        setLoaded(true);
+        search();
+    }
+    if (loaded) {
+        return (
+            <div className="search">
+                <form onSubmit={handleSubmit}>
+                    <input className="search-bar" placeholder="Enter a word to search" size="60" type="search" autoFocus={true} onChange={handleWord} />
+                </form>
+                <Results results={results} />
+            </div>
+        );
+    } else {
+        load()
+        return "Loading...";
+    }
 }
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Phonetics(props) {
     return (
-        <div className="phonetics">
-            <a href={props.phonetic.audio} target="blank">Listen</a>
-            {props.phonetic.text}
-        </div>
+      <div className="phonetics">
+        <a href={props.phonetic.audio} target="blank">
+          🔉
+        </a>
+        {props.phonetic.text}
+      </div>
     );
 }
