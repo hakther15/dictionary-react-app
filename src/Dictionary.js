9193c8489c29d8fd0645bcfec8db7472660a481f
hakther15 <92819734+hakther15@users.noreply.github.com>
2022-02-28 22:36:02 -0500

    attempt to add photos in dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
+
 
 export default function Dictionary(props) {
-    let [word, setWord] = useState(props.defaultKeyword);
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+    }
+
+
     function search() {
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-        axios.get(apiUrl).then(handleResponse);
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        const pexelsApiKey ="563492ad6f9170000100000144d1b1378cf0414597b47d6fd6f2bb48";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -21,8 +34,8 @@ export default function Dictionary(props) {
         search();
     }
 
-    function handleWord(event) {
-        setWord(event.target.value);
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
 
     }
 
@@ -35,9 +48,10 @@ export default function Dictionary(props) {
             <div className="search">
                 <h4>What would you like to look up?</h4>
                 <form onSubmit={handleSubmit}>
-                    <input className="search-bar" placeholder="Enter a word to search" size="30" type="search" autoFocus={true} onChange={handleWord} defaultValue={props.defaultKeyword}/>
+                    <input className="search-bar" placeholder="Enter a word to search" size="30" type="search" autoFocus={true} onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
                 </form>
                 <Results results={results} />
+                <Photos photos={photos} />
             </div>
         );
     } else {
